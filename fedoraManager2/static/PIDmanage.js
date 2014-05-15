043a921d16e052b5c5049608842dda19c176dfac
root <root@mitten.(none)>
2014-05-15 17:34:35 +0000

    don't touch it, working with datatables

--- fedoraManager2/static/PIDmanage.js
+++ fedoraManager2/static/PIDmanage.js
@@ -1,14 +1,9 @@
 // JS for creating tables based on user selectedPIDs
 
-
-
 // server-side table manipulation for PID managing
-$(document).ready(function(){	
+$(document).ready(function(){			
 	$('#example').DataTable( {		
-	    bserverSide: true,	    	    	    	    	    	    
-	    ajax: {	        
-	        url: 'http://162.243.93.130/cgi-bin/php_dt.php',
-	        type: 'POST'
-	    }
+	    "serverSide": true,
+		"sAjaxSource": 'http://162.243.93.130/cgi-bin/php_dt.php'				    
 	} );	
 });
