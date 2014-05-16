70aabff2836c833d1d89b306c775e495a4a80f84
root <root@mitten.(none)>
2014-05-16 17:39:54 +0000

    pause

--- fedoraManager2/actions/actions.py
+++ fedoraManager2/actions/actions.py
@@ -24,7 +24,7 @@ def celeryTaskFactory(**kwargs):
 	
 	step = 1	
 	while userPID_pag.page < (userPID_pag.pages + 1):
-
+		
 		# iterate through PIDs in userPID_pag page
 		for PID in userPID_pag.items:			
 			print "Operating on PID:",PID.PID," / Step:",step		
@@ -49,6 +49,22 @@ def sampleTask(job_package):
 	print "Starting sampleTask",job_package['step']
 	
 	# delay for testing	
+	time.sleep(.25)	
+	
+	# update taskHand about task	
+	redisHandles.r_job_handle.set("task{step}_job_num{job_num}".format(step=job_package['step'],job_num=job_package['job_num']), "triggered")	
+
+	# return results
+	return 40 + 2
+
+
+@celery.task()
+def sampleFastTask(job_package):
+
+	username = job_package['username']
+	print "Starting sampleFastTask",job_package['step']
+	
+	# delay for testing	
 	# time.sleep(.25)	
 	
 	# update taskHand about task	
@@ -56,6 +72,8 @@ def sampleTask(job_package):
 
 	# return results
 	return 40 + 2
+
+
 
 
 
--- fedoraManager2/static/PIDmanage.js
+++ fedoraManager2/static/PIDmanage.js
@@ -1,9 +1,53 @@
 // JS for creating tables based on user selectedPIDs
 
-$(document).ready(function(){			
-	$('#example').DataTable( {		
+
+
+$(document).ready(function(){	
+
+	var selected = [];
+	
+	$('#example').dataTable( {		
 	    "serverSide": true,		
-		"ajax": 'http://162.243.93.130/cgi-bin/php_simple.php',						
+	    "searchCols":[
+	    	null,
+	    	{"search":"smalluser", "escapeRegex":false}
+	    ],
+		"ajax": 'http://162.243.93.130/cgi-bin/php_simple.php',		
+		"rowCallback": function( row, data, displayIndex ) {
+            if ( $.inArray(data.DT_RowId, selected) !== -1 ) {
+                $(row).addClass('selected');
+                console.log("fired");
+            }
+        }
 	} );	
+
+	$('#example tbody').on('click', 'tr', function () {
+        var id = this.id;
+        var index = $.inArray(id, selected);
+ 
+        if ( index === -1 ) {
+            selected.push( id );
+        } else {
+            selected.splice( index, 1 );
+        }
+ 
+        $(this).toggleClass('selected');
+    } );
+
+	// simple click example
+	// $('#example tbody').on('click', 'tr', function () {
+ //        var name = $('td', this).eq(0).text();
+ //        alert( 'You clicked on '+name+'\'s row' );
+ //    } );
+
+	// click selected
+	    // $('#example tbody').on( 'click', 'tr', function () {
+	    //     $(this).toggleClass('selected');
+	    // } );
+	 
+	    // $('#button').click( function () {
+	    //     alert( table.rows('.selected').data().length +' row(s) selected' );
+	    // } );
+
 });
 
